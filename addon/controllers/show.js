7f1f9181458df840f8a99128d29ed06188e372d4
Jaco Joubert <jaco@hey.com>
2022-10-28 15:33:36 -0400

    Allow users to register custom extensions

--- addon/controllers/show.js
+++ addon/controllers/show.js
@@ -6,8 +6,14 @@ import config from 'ember-get-config';
 
 export default Controller.extend({
   fieldGuideConfig: config['field-guide'],
-  renderedContent: computed('model.content', function () {
-    const converter = new showdown.Converter({ extensions: ['intro'] });
-    return converter.makeHtml(this.model.content);
-  }),
+  renderedContent: computed(
+    'fieldGuideConfig.extensions',
+    'model.content',
+    function () {
+      const converter = new showdown.Converter({
+        extensions: this.fieldGuideConfig.extensions || [],
+      });
+      return converter.makeHtml(this.model.content);
+    }
+  ),
 });
